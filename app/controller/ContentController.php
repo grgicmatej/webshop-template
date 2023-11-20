beae1bff539f48fb2c3cf4d08893a15be6fc99b1
Matej Grgić <matej.grgic@undabot.com>
2023-11-20 09:11:05 +0100

    feat Content management

--- app/controller/ContentController.php
+++ app/controller/ContentController.php
@@ -2,6 +2,47 @@
 
 declare(strict_types=1);
 
-class ContentController
+class ContentController extends SecurityController
 {
+    public function editContent($id): void
+    {
+        $this->isAdmin();
+        $view = new View();
+        $view->render('public/sadrzaj',
+            [
+                'content' =>(array) Content::getOneContent(intval($id)),
+                'page' => $_GET['page']
+            ]);
+    }
+
+    public function editContentImages($id): void
+    {
+        $this->isAdmin();
+        $view = new View();
+        $view->render('public/slike',
+            [
+                'content' =>(array) Content::getOneContentImages(intval($id)),
+                'page' => $_GET['page']
+            ]);
+    }
+
+    public function updateContent(): void
+    {
+        $this->isAdmin();
+        Content::updateContent();
+        header( 'Location:'.App::config('url').'/Routes/index/'.Request::post('page'));
+    }
+
+    /** @throws Exception */
+    public function updateContentImages(): void
+    {
+        $this->isAdmin();
+        Upload::UploadPhoto();
+        if (Upload::GetFileName() !== NULL){
+            Content::updateContentImages(Upload::GetFileName());
+        } else {
+            throw new Exception('upload error');
+        }
+        header( 'Location:'.App::config('url').'/Routes/index/'.Request::post('page'));
+    }
 }
--- app/core/Content.php
+++ app/core/Content.php
@@ -33,4 +33,45 @@ class Content
 
         return $fetchedData;
     }
+
+    public static function getOneContent(int $id)
+    {
+        $db = Db::getInstance();
+        $stmt = $db->prepare('SELECT * FROM content WHERE content.id = :id');
+        $stmt->bindValue('id', $id);
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+    public static function getOneContentImages(int $id)
+    {
+        $db = Db::getInstance();
+        $stmt = $db->prepare('SELECT * FROM content_images WHERE content_images.id = :id');
+        $stmt->bindValue('id', $id);
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+    public static function updateContent(): void
+    {
+        $db  = Db::getInstance();
+        $stmt = $db->prepare('UPDATE content SET content.text=:text, content.text_en=:text_en WHERE content.id=:id');
+        $stmt->bindValue('text', Request::post('text'));
+        $stmt->bindValue('text_en', Request::post('text_en'));
+        $stmt->bindValue('id', Request::post('id'));
+        $stmt->execute();
+    }
+
+    public static function updateContentImages(string $image): void
+    {
+        $db  = Db::getInstance();
+        $stmt = $db->prepare('UPDATE content_images SET content_images.image=:image, content_images.title=:title, content_images.title_en=:title_en WHERE content_images.id=:id');
+        $stmt->bindValue('image', $image);
+        $stmt->bindValue('title', Request::post('title'));
+        $stmt->bindValue('title_en', Request::post('title_en'));
+        $stmt->bindValue('id', Request::post('id'));
+        $stmt->execute();
+    }
 }
